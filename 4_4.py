58d2c91c69d638365cc59e022aedecbe7da88161
vicktor301211 <iva301211@gmail.com>
2025-03-15 15:38:25 +0300

    Текст и самолёты
    Надо доделать дз с самолётами.

--- 4_4.py
+++ 4_4.py
@@ -1,10 +1,16 @@
+from all_colors import *
 import pygame
 pygame.init()
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Моя игра")
-BACKGROUND = (255, 255, 255)
+BACKGROUND = YELLOW
 screen.fill(BACKGROUND)
+
+my_font = pygame.font.SysFont("Arial", 32)
+my_text = my_font.render("Потрачено!", True, (0,0,0), (BACKGROUND))
+screen.blit(my_text, (100, 100))
+
 FPS = 60
 clock = pygame.time.Clock()
 running = True
@@ -15,7 +21,7 @@ while running:
             running = False
     #Основная логика
     #Отрисовка объектов
-    screen.fill(BACKGROUND)
+    #screen.fill(BACKGROUND)
     pygame.display.flip()
     clock.tick(FPS)
 pygame.quit()
